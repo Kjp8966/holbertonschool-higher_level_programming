9367239ba0ff279889612e1b202075b5cd0661d6
kenny peery <kjpeerry369@yahoo.com>
2023-10-25 00:09:00 +0000

    get all states2

--- python-object_relational_mapping/0-select_states.sql
+++ python-object_relational_mapping/0-select_states.sql
@@ -4,6 +4,6 @@ USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states (
        id INT NOT NULL AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
-PRIMARY KEY (id)
+       PRIMARY KEY (id)
 	);
-	INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
