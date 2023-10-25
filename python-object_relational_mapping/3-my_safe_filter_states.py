8199f5f6e8853b38ed68f5cbd8968245b48bbbe0
kenny peery <kjpeerry369@yahoo.com>
2023-10-25 01:04:39 +0000

    SQL injection2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
-import sys
+"""Lists states"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    # Get the command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name)
-
-    # Create a cursor object to execute queries
-cursor = db.cursor()
-
-    # Prepare the SQL query with placeholders
-sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query with the state name as a parameter
-cursor.execute(sql_query, (state_name,))
-
-    # Fetch all the rows returned by the query
-rows = cursor.fetchall()
-
-    # Display the results
-for row in rows:
-    print(row)
-
-    # Close the cursor and database connection
-cursor.close()
-db.close()
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                passwd=argv[2], db=argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+                    (argv[4], ))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
